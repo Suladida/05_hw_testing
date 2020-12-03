639cb2ef69395adb9bc25485f23173f45b4417de
Robyn Tyler <robynstyler@outlook.com>
2020-12-03 16:08:41 +0000

    Homework complete.

---
+++ specs/card_game_tests.py
@@ -0,0 +1,26 @@
+import unittest
+from src.card import Card
+from src.card_game import CardGame
+
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.card1 = Card("Hearts", 3)
+        self.card2 = Card("Clubs", 4)
+        self.card3 = Card("Clubs", 1)
+        self.cardgame = CardGame()
+        self.cards = (self.card1, self.card2)
+
+    def test_check_for_ace__false(self):
+        self.cardgame.check_for_ace(self.card1)
+        self.assertEqual(False, self.cardgame.check_for_ace(self.card1))
+
+    def test_check_for_ace__true(self):
+        self.cardgame.check_for_ace(self.card3)
+        self.assertEqual(True, self.cardgame.check_for_ace(self.card3))
+        
+    def test_highest_card(self):
+        result = self.cardgame.highest_card(self.card1, self.card2)
+        self.assertEqual(self.card2, result)
+
+    def test_cards_total(self):
+        self.assertEqual("You have a total of 7", self.cardgame.cards_total(self.cards))
